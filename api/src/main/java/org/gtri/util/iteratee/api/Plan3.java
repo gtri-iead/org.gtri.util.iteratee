be11d31ec7c6247303c7a03e795cbd923882b60d
Lance Gatlin <lance.gatlin@gtri.gatech.edu>
2012-12-04 05:19:11 -0500

    Added override

--- api/src/main/java/org/gtri/util/iteratee/api/Plan3.java
+++ api/src/main/java/org/gtri/util/iteratee/api/Plan3.java
@@ -36,7 +36,7 @@ public interface Plan3<I1,I2, O> extends Enumerator<O>, Iterable<Plan3.State.Res
   
   Iteratee<I2, O> iteratee();
 
-  Iterator<Plan3.State.Result<I1,I2,O>> iterator();
+  @Override Iterator<Plan3.State.Result<I1,I2,O>> iterator();
   Iterator<Plan3.State.Result<I1,I2,O>> iterator(IssueHandlingCode issueHandlingCode);
 //  <U> U runFoldLeft(U u, Function2<U, Plan3.State.Result<I1,I2,O>,U> f);
 //  void runForEach(Function1<Plan3.State.Result<I1,I2,O>,?> f);
