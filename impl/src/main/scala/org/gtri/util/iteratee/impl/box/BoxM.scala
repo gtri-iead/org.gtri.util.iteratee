2a61599295bf2e918027ab54b59d2c550a3139cb
Lance Gatlin <lance.gatlin@gtri.gatech.edu>
2012-12-26 04:50:16 -0500

    Removed commented out code and fixed licenses.

--- impl/src/main/scala/org/gtri/util/iteratee/impl/box/BoxM.scala
+++ impl/src/main/scala/org/gtri/util/iteratee/impl/box/BoxM.scala
@@ -1,3 +1,24 @@
+/*
+    Copyright 2012 Georgia Tech Research Institute
+
+    Author: lance.gatlin@gtri.gatech.edu
+
+    This file is part of org.gtri.util.iteratee library.
+
+    org.gtri.util.iteratee library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    org.gtri.util.iteratee library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with org.gtri.util.iteratee library. If not, see <http://www.gnu.org/licenses/>.
+
+*/
 package org.gtri.util.iteratee.impl.box
 
 import language.higherKinds
@@ -125,246 +146,3 @@ sealed trait BoxM[M[+_],+A] {
     )
   }
 }
-
-
-//object Box {
-//  type OptW[+W, +A] = Writer[W, Option[A]]
-//  type LazyOptW[+W, +A] = Lazy[OptW[W,A]]
-//
-//  def empty[W,A] : Box[W,A] = NoGo
-//  def apply[W,A](a : A) : Box[W,A] = Go(a)
-//  def fromOption[W,A](opt : Option[A]) : Box[W,A] = {
-//    if(opt.isDefined) {
-//      Go(opt.get)
-//    } else {
-//      NoGo
-//    }
-//  }
-//  def recover[W,A](recoverable: => Lazy[OptW[W,A]]) : Box[W,A] = Recover(recoverable)
-//}
-//
-//case object NoGo extends Box[Nothing,Nothing] {
-//  import Box.LazyOptW
-//
-//  override def isNoGo : Boolean = true
-//
-//  def fold[X](ifNoGo: => X, ifRecover: LazyOptW[Nothing,Nothing] => X, ifGo: Nothing => X) : X = ifNoGo
-//}
-//
-//final case class Recover[+W, +A](override val recoverable : Box.LazyOptW[W,A]) extends Box[W, A] {
-//  import Box.LazyOptW
-//
-//  override def isRecover : Boolean = true
-//
-//  def fold[X](ifNoGo: => X, ifRecover: LazyOptW[W,A] => X, ifGo: A => X) : X = ifRecover(recoverable)
-//}
-//
-//object Recover {
-//  import Box.OptW
-//  def apply[W,A](recoverable: => OptW[W,A]) : Recover[W,A] = Recover(Lazy(recoverable))
-//}
-//
-//final case class Go[+W, +A] (override val get : A) extends Box[W,A] {
-//  import Box.LazyOptW
-//
-//  override def isGo : Boolean = true
-//
-//  def fold[X](ifNoGo: => X, ifRecover: LazyOptW[W,A] => X, ifGo: A => X) : X = ifGo(get)
-//}
-//
-//
-//sealed trait Box[+W,+A] {
-//  import Box.OptW
-//  import Box.LazyOptW
-//
-//  // Override in inherited classes
-//  def fold[X](ifNoGo: => X, ifRecover: LazyOptW[W,A] => X, ifGo: A => X) : X
-//  def isNoGo : Boolean = false
-//  def isRecover : Boolean = false
-//  def isGo : Boolean = false
-//  def get : A = throw new NoSuchElementException
-//  def recoverable : LazyOptW[W,A] = throw new IllegalStateException
-//
-//  def recover[WW >: W](implicit M: Monoid[WW]) : OptW[WW,A] = fold(
-//    ifNoGo = { Writer(M.zero,None) },
-//    ifRecover = { recoverable => recoverable.value },
-//    ifGo = { a => Writer(M.zero, Some(a)) }
-//  )
-//
-//  /**
-//   * Convert to an option. Go maps to Some, recover and nogo maps to None
-//   * @return an option
-//   */
-//  def toOption : Option[A] = fold(
-//    ifNoGo = { None },
-//    ifRecover = { _ => None },
-//    ifGo = { a => Some(a) }
-//  )
-//
-//  def flatMap[WW >: W,B](f: A => Box[WW,B])(implicit M: Monoid[WW]) : Box[WW,B] = fold(
-//    ifNoGo = { Box.empty },
-//    ifRecover = {
-//      recoverable =>
-//        Recover(Lazy({
-//          recoverable.value.flatMap { oa =>
-//            if(oa.isDefined) {
-//              f(oa.get).recover
-//            } else {
-//              Writer(M.zero, None)
-//            }
-//          }
-//          //            val opt = recoverable.value.value
-//          //            val log = recoverable.value.written
-//          //            if(opt.isDefined) {
-//          //              f(opt.get).recover :++> log
-//          //            } else {
-//          //              Writer(log, None)
-//          //            }
-//        }))
-//    },
-//    ifGo = { a => f(a) }
-//  )
-//
-//  def map[WW >: W,B](f: A => B)(implicit M: Monoid[WW]) : Box[WW,B] = fold(
-//    ifNoGo = { Box.empty },
-//    ifRecover = {
-//      recoverable =>
-//        Recover(Lazy({
-//          recoverable.value.flatMap { oa =>
-//            if(oa.isDefined) {
-//              Writer(M.zero, Some(f(oa.get)))
-//            } else {
-//              Writer(M.zero, None)
-//            }
-//          }
-//          //            val opt = recoverable.value.value
-//          //            val log = recoverable.value.written
-//          //            if(opt.isDefined) {
-//          //              Writer(log, Some(f(opt.get)))
-//          //            } else {
-//          //              Writer(log, None)
-//          //            }
-//        }))
-//    },
-//    ifGo = { a => Box(f(a)) }
-//  )
-//
-//
-//  def foreach[U](f: A => U) {
-//    fold(
-//      ifNoGo = { () },
-//      ifRecover = { _ => () },
-//      ifGo = { a => f(a) }
-//    )
-//  }
-//}
-
-
-//object Box {
-//
-//  def empty[A] : Box[A] = NoGo()
-//  def apply[A](a : A) : Box[A] = Go(a)
-//  def fromOption[A](opt : Option[A]) : Box[A] = {
-//    if(opt.isDefined) {
-//      Go(opt.get)
-//    } else {
-//      NoGo()
-//    }
-//  }
-//  def recover[A](recoverable: => Lazy[OptLogWriter[A]]) : Box[A] = Recover(recoverable)
-//}
-//
-//final case class NoGo[+A]() extends Box[A] {
-//
-//  override def isNoGo : Boolean = true
-//
-//  def fold[X](ifNoGo: => X, ifRecover: Lazy[OptLogWriter[A]] => X, ifGo: A => X) : X = ifNoGo
-//}
-//
-//final case class Recover[+A](override val recoverable : Lazy[OptLogWriter[A]]) extends Box[A] {
-//
-//  override def isRecover : Boolean = true
-//
-//  def fold[X](ifNoGo: => X, ifRecover: Lazy[OptLogWriter[A]] => X, ifGo: A => X) : X = ifRecover(recoverable)
-//}
-//
-//object Recover {
-//  def apply[A](recoverable: => OptLogWriter[A]) : Recover[A] = Recover(Lazy(recoverable))
-//}
-//
-//final case class Go[+A] (override val get : A) extends Box[A] {
-//  override def isGo : Boolean = true
-//
-//  def fold[X](ifNoGo: => X, ifRecover: Lazy[OptLogWriter[A]] => X, ifGo: A => X) : X = ifGo(get)
-//}
-//
-//
-//sealed trait Box[+A] {
-//
-//  // Override in inherited classes
-//  def fold[X](ifNoGo: => X, ifRecover: Lazy[OptLogWriter[A]] => X, ifGo: A => X) : X
-//  def isNoGo : Boolean = false
-//  def isRecover : Boolean = false
-//  def isGo : Boolean = false
-//  def get : A = throw new NoSuchElementException
-//  def recoverable : Lazy[OptLogWriter[A]] = throw new IllegalStateException
-//
-//  def recover : OptLogWriter[A] = fold(
-//    ifNoGo = { OptLogWriter.empty },
-//    ifRecover = { recoverable => recoverable.value },
-//    ifGo = { a => OptLogWriter(a) }
-//  )
-//
-//  /**
-//   * Convert to an option. Go maps to Some, recover and nogo maps to None
-//   * @return an option
-//   */
-//  def toOption : Option[A] = fold(
-//    ifNoGo = { None },
-//    ifRecover = { _ => None },
-//    ifGo = { a => Some(a) }
-//  )
-//
-//  def flatMap[B](f: A => Box[B]) : Box[B] = fold(
-//    ifNoGo = { Box.empty },
-//    ifRecover = {
-//      recoverable =>
-//        Recover(Lazy({
-//          val opt = recoverable.value.value
-//          val log = recoverable.value.written
-//          if(opt.isDefined) {
-//            f(opt.get).recover :++> log
-//          } else {
-//            LogWriter(log, None)
-//          }
-//        }))
-//    },
-//    ifGo = { a => f(a) }
-//  )
-//
-//  def map[B](f: A => B) : Box[B] = fold(
-//    ifNoGo = { Box.empty },
-//    ifRecover = {
-//      recoverable =>
-//        Recover(Lazy({
-//          val opt = recoverable.value.value
-//          val log = recoverable.value.written
-//          if(opt.isDefined) {
-//            LogWriter(log, Some(f(opt.get)))
-//          } else {
-//            LogWriter(log, None)
-//          }
-//        }))
-//    },
-//    ifGo = { a => Box(f(a)) }
-//  )
-//
-//
-//  def foreach[U](f: A => U) {
-//    fold(
-//      ifNoGo = { () },
-//      ifRecover = { _ => () },
-//      ifGo = { a => f(a) }
-//    )
-//  }
-//}
